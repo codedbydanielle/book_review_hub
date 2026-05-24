8d9ba64619fbd039dad3dfc36be3cc2aedd9d5fd
Danielle Deacon <deacondanielle463@gmail.com>
2026-05-24 16:55:10 -0500

    Add usernames and improve navbar profile display

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  
+
+  validates :username, presence: true, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
